28a8593f194089a5a43dee4565dd89baad2a1270
unknown <thomas123un@gmail.com>
2026-09-17 22:01:44 -0500

    agregando Endpoints

--- api.py
+++ api.py
@@ -12,5 +12,18 @@ productos = [
     }
 ]
 
+@app.route('/productos', methods=['GET'])
+def obtener_productos():
+    return jsonify(productos)
+
+@app.route('/productos/<int:id>', methods=['GET'])
+def obtener_producto(id):
+
+    for producto in productos:
+        if producto["id"] == id:
+            return jsonify(producto)
+
+    return jsonify({"error": "Producto no encontrado"}), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
