77d33b5a50ba08ac1157715d58d9ccfc477c62c6
unknown <thomas123un@gmail.com>
2026-09-18 22:01:15 -0500

    agregacion de endpoints

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -24,6 +24,55 @@ def obtener_producto(id):
             return jsonify(producto)
 
     return jsonify({"error": "Producto no encontrado"}), 404
+
+@app.route('/productos', methods=['POST'])
+def crear_producto():
+
+    datos = request.get_json()
+
+    if len(productos) > 0:
+        nuevo_id = max(p["id"] for p in productos) + 1
+    else:
+        nuevo_id = 1
+
+        nuevo_producto = {
+        "id": nuevo_id,
+        "nombre": datos["nombre"],
+        "precio": datos["precio"],
+        "cantidad": datos["cantidad"],
+        "categoria": datos["categoria"]
+
+    }
+
+    productos.append(nuevo_producto)
+
+    return jsonify(nuevo_producto), 201
+
+@app.route('/productos/<int:id>', methods=['PUT'])
+def modificar_producto(id):
+    datos = request.get_json()
+
+    for producto in productos:
+        if producto["id"] == id:
+
+            producto["nombre"] = datos.get("nombre", producto["nombre"])
+            producto["precio"] = datos.get("precio", producto["precio"])
+            producto["cantidad"] = datos.get("cantidad", producto["cantidad"])
+            producto["categoria"] = datos.get("categoria", producto["categoria"])
+
+            return jsonify(producto)
+        
+        return jsonify({"error": "Producto no encontrado"}), 404
+    
+@app.route('/productos/<int:id>', methods=['DELETE'])
+def eliminar_producto(id):
+    for producto in productos:
+        if producto["id"] == id:
+            productos.remove(producto)
+            return jsonify({"mensaje": "Producto eliminado exitosamente"})
+        
+    return jsonify({"error": "Producto no encontrado"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
